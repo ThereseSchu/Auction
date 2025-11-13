8b1bc81ad7a0dd89d5a69ed67635d0ed287a1afe
Morten <MERL@ITU.DK>
2025-11-13 12:23:14 +0100

    Implemented ARGS and port selection in server

--- server/server.go
+++ server/server.go
@@ -3,8 +3,11 @@ package main
 import (
 	proto "ITUserver/grpc"
 	"context"
+	"fmt"
 	"log"
 	"net"
+	"os"
+	"strconv"
 
 	"google.golang.org/grpc"
 )
@@ -19,18 +22,21 @@ func (s *ITU_databaseServer) GetMessages(ctx context.Context, in *proto.Empty) (
 }
 
 func main() {
+	ID, _ := strconv.ParseInt(os.Args[1], 10, 32)
 	// we input args when running the server, like nodes in last assignment
 	// args determines what port server is started on
 
 	// every time server gets a new bid, increment logical clock and update replica server
 	// physical time needs to be transfered to replica at certain pysical time intervals, alongside a logical timestamp update
 	server := &ITU_databaseServer{messages: []string{}}
-	server.start_server()
+	server.start_server(int32(ID))
 }
 
-func (s *ITU_databaseServer) start_server() {
+func (s *ITU_databaseServer) start_server(ID int32) {
+	port := fmt.Sprintf(":800%d", ID)
+
 	grpcserver := grpc.NewServer()
-	listener, err := net.Listen("tcp", ":8000")
+	listener, err := net.Listen("tcp", port)
 
 	if err != nil {
 		log.Fatalf("SERVER WONT WORK")
