00db7d971a37e64d683970a39f3bd813662e7966
Joakim-David <Jpre@itu.dk>
2025-11-20 14:55:51 +0100

    Auction and start Auction

--- server/server.go
+++ server/server.go
@@ -15,6 +15,16 @@ import (
 type ITU_databaseServer struct {
 	proto.UnimplementedITUDatabaseServer
 	messages []string
+	auction Auction
+}
+
+type Auction struct {
+	ongoing bool
+	highestBid int64
+	timestamp int64
+	highestBidder string
+	endTime int64
+
 }
 
 func (s *ITU_databaseServer) GetMessages(ctx context.Context, in *proto.Empty) (*proto.Message, error) {
@@ -26,10 +36,24 @@ func main() {
 	// we input args when running the server, like nodes in last assignment
 	// args determines what port server is started on
 
+	var auction = Auction{
+} 
+
 	// every time server gets a new bid, increment logical clock and update replica server
 	// physical time needs to be transfered to replica at certain pysical time intervals, alongside a logical timestamp update
 	server := &ITU_databaseServer{messages: []string{}}
 	server.start_server(int32(ID))
+}
+
+func (s *ITU_databaseServer) placeBid(ctx context.Context, bid *proto.Bid) {
+	var id = bid.Id
+	var timestamp = bid.Timestamp
+	var bidAmount = bid.Bid
+
+	if s.auction.highestBid == 0 {
+		s.startAuction(id, timestamp, bidAmount)
+		log.Println("Auction startet")
+	}
 }
 
 func (s *ITU_databaseServer) start_server(ID int32) {
@@ -51,6 +75,17 @@ func (s *ITU_databaseServer) start_server(ID int32) {
 
 func (s *ITU_databaseServer) PlaceBid(ctx context.Context, bid *proto.Empty) (*proto.Bid, error) {
 
+}
+
+func (s *ITU_databaseServer) startAuction(name string, timestamp int64, bidAmount int64){
+	s.auction = Auction{
+		ongoing: true,
+		highestBid: bidAmount,
+		timestamp: timestamp,
+		highestBidder: name,
+		endTime: timestamp + 100,
+		
+	}
 }
 
 //
